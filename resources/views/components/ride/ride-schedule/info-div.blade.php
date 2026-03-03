d453df3f730beab07ad9f95176f8c1d90c938494
warmkatahira <t.katahira@warm.co.jp>
2026-03-03 14:24:24 +0900

    20260303_1

--- resources/views/components/ride/ride-schedule/info-div.blade.php
+++ resources/views/components/ride/ride-schedule/info-div.blade.php
@@ -3,7 +3,7 @@
     'value',
 ])
 
-<div class="flex flex-row items-center">
+<div class="col-span-6 flex flex-row items-center">
     <p class="w-1/3 py-2 bg-theme-main text-white pl-2 border border-theme-main">{{ $label }}</p>
     <p class="w-2/3 py-2 pl-2 border border-black">{{ $value }}</p>
 </div>
--- resources/views/ride/ride_driver_candidate/update.blade.php
+++ resources/views/ride/ride_driver_candidate/update.blade.php
@@ -2,14 +2,14 @@
     <x-page-back :url="session('back_url_1')" />
     <div class="mt-5">
         <div class="flex flex-col gap-3">
-            <div class="flex flex-col gap-3 w-1/2 bg-white p-3 border border-gray-400">
+            <div class="grid grid-cols-12 gap-3 bg-white p-3 border border-gray-400">
                 <x-ride.ride-schedule.info-div label="送迎日" :value="CarbonImmutable::parse($ride->ride_schedule)->isoFormat('YYYY年MM月DD日')" />
                 <x-ride.ride-schedule.info-div label="送迎ステータス" :value="$ride->ride_status->ride_status" />
                 <x-ride.ride-schedule.info-div label="ルート区分" :value="$ride->route_type->route_type" />
                 <x-ride.ride-schedule.info-div label="ルート名" :value="$ride->route_name" />
                 <x-ride.ride-schedule.info-div label="車両種別" :value="$ride->vehicle_category->vehicle_category" />
             </div>
-            <div class="flex mt-3">
+            <div class="flex">
                 <button id="add_ride_driver_candidate_btn" type="button" class="btn bg-green-600 text-white p-3 ml-left"><i class="las la-plus la-lg mr-1"></i>ドライバー追加</button>
             </div>
             <form method="POST" action="{{ route('ride_driver_candidate_update.update') }}" id="ride_driver_candidate_form">
